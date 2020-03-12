76f58e124588d9e4d2e7c7fa78019626006be5ec
Sallivanchuk <61000905+Sallivanchuk@users.noreply.github.com>
2020-03-12 10:16:39 +0500

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,74 @@
+const numDivs = 36;
+const maxHits = 10;
+
+
+
+let hits = 0;
+let firstHitTime = 0;
+
+
+
+function round(){
+  $('.target').removeClass('target');
+  $('.miss').removeClass('miss');
+
+  let divSelector = randomDivId();
+  $(divSelector).addClass("target");
+  $(divSelector).text(hits + 1);
+
+  if (hits === 1){
+    firstHitTime = getTimestamp();
+    console.log(firstHitTime);
+  }
+  if (hits === maxHits){
+    endGame();
+  }
+}
+
+
+
+
+function endGame(){
+  hits = 0;
+  $('.game-field, #button-start').hide();
+
+  let totalPlayedMillis = getTimestamp() - firstHitTime;
+  let totalPlayedSeconds = Number(totalPlayedMillis / 1000).toPrecision(3);
+  $("#total-time-played").text(totalPlayedSeconds);
+
+  $("#win-message").removeClass("d-none");
+}
+
+
+
+function handleClick(event){
+  let target = $(event.target);
+  if ($(event.target).hasClass('target')) {
+  target.text('')
+  hits = hits + 1;
+  round();
+  } else {
+  $(event.target).hasClass('miss')
+  }
+}
+
+
+
+$(function(){
+  $('#button-start').click(function(){
+    round();
+    $('.game-field').click(handleClick);
+  });
+});
+
+
+
+function init(){
+  $("#button-reload").click(function(){
+    location.reload();
+  });
+}
+
+
+
+$(document).ready(init);
